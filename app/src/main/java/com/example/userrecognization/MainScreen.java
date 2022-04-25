bfeb840d889b21b323364da5f9ed99ba244d32fc
VMutthu <47241355+VMutthu@users.noreply.github.com>
2022-04-25 11:49:59 +0530

    modify the mainfest

---
+++ app/src/main/java/com/example/userrecognization/MainScreen.java
@@ -0,0 +1,35 @@
+package com.example.userrecognization;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainScreen extends AppCompatActivity {
+    AppCompatButton adduser,validate_user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        adduser=(AppCompatButton) findViewById(R.id.adduser);
+        validate_user=(AppCompatButton) findViewById(R.id.validateuser);
+        adduser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(new Intent(MainScreen.this,MainActivity.class));
+            }
+        });
+        validate_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(new Intent(MainScreen.this,MainActivity.class));
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/userrecognization/token_git.java
@@ -0,0 +1,5 @@
+package com.example.userrecognization;
+
+public class token_git {
+    String token="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";
+}
